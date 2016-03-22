8131d3268a952a399202201978a13bf8a4d453de
PraveenSubramaniyam <praveenatceg@gmail.com>
2016-03-21 18:56:57 -0700

    Crypto

--- char.c
+++ char.c
@@ -2,8 +2,11 @@
 
 void main()
 {
-  char ptr[] = "hello";
-  printf("size:%d",sizeof(ptr));
+ // char ptr[] = "hello";
+  //printf("size:%d",sizeof(ptr));
+
+  char ptr[] = "1234";
+  printf("%d-%d\n",ptr[0],ptr[0]-'0');
 }
 
 
