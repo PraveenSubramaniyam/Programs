53a91164eac0f463353a3f789f2c8bf46733cdb2
PraveenSubramaniyam <praveenatceg@gmail.com>
2016-03-21 20:43:40 -0700

    rsa commit

--- rsa.h
+++ rsa.h
@@ -4,10 +4,13 @@ typedef struct {
   int noOfDigits;
 }myInteger;
 
+
+void trimSlash(char *source);
 void memsetMyIntegertoZero(myInteger *myInt);
-void leftshift(myInteger *a, int b);
+void generateMyInteger(myInteger *num, char *str);
+void leftshift(myInteger *num, int count);
 void stripZero(myInteger *num);
 void add(myInteger num1,int aCounter, myInteger num2,int bCounter, myInteger *result,int cCounter,int carry);
 void divide(myInteger *a , myInteger *b, myInteger *c,myInteger *m);
 int ComparemyInteger(myInteger int1, myInteger int2);
-int  subtract(myInteger *a, int aCounter , myInteger *b, int bCounter,myInteger *c, int cCounter , int borrow);
+void subtract(myInteger *num1, int num1Count, myInteger *num2, int num2Count, myInteger *result, int resCount, int borrow);
